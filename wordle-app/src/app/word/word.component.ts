dd99b1d8285f8f9032864a748efea72752116e0e
pawsie <pawsielee@gmail.com>
2022-08-03 12:57:53 +0800

    add flip animation for whole word

--- wordle-app/src/app/word/word.component.ts
+++ wordle-app/src/app/word/word.component.ts
@@ -1,4 +1,4 @@
-import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, keyframes, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Word } from './wordModel';
 
@@ -9,18 +9,39 @@ import { Word } from './wordModel';
   animations: [
     trigger('shake', [     
      
-      transition('true <=> false', [
-        animate('0.5s', keyframes([
-          style({transform: 'translateX(-10px)', offset: 0}),
-          style({transform: 'translateX(20px)', offset: 0.2}),
-          style({transform: 'translateX(-20px)', offset: 0.4}),
-          style({transform: 'translateX(20px)', offset: 0.6}),
-          style({transform: 'translateX(-20px)', offset: 0.8}),
-          style({transform: 'translateX(10px)', offset: 1}),
-        ]))
-      ]),
+      // transition('true <=> false', [
+      //   animate('0.5s', keyframes([
+      //     style({transform: 'translateX(-10px)', offset: 0}),
+      //     style({transform: 'translateX(20px)', offset: 0.2}),
+      //     style({transform: 'translateX(-20px)', offset: 0.4}),
+      //     style({transform: 'translateX(20px)', offset: 0.6}),
+      //     style({transform: 'translateX(-20px)', offset: 0.8}),
+      //     style({transform: 'translateX(10px)', offset: 1}),
+      //   ]))
+      // ]),
     ]),
-  
+
+    trigger('letterFlip', [        
+     
+      transition('true <=> false', [        
+          // query(":leave", 
+            // [stagger(400,
+              animate('0.5s', 
+                keyframes([
+                  style({transform: 'scaleY(60%)', offset: 0}),
+                  style({transform: 'scaleY(30%)', offset: 0.2}),
+                  style({transform: 'scaleY(0%)', offset: 0.4}),
+                  style({transform: 'scaleY(30%)', offset: 0.6}),
+                  style({transform: 'scaleY(60%)', offset: 0.8}),
+                  style({transform: 'scaleY(100%)', offset: 1}),
+                ])
+              )
+            // )],
+            // {optional: true}
+          // ),        
+      ]),
+
+    ]),
   ]
 })
 export class WordComponent implements OnInit {
@@ -31,9 +52,11 @@ export class WordComponent implements OnInit {
   }
 
   shakeTrigger = true;
+  flipTrigger = true;
 
   shake() {
     this.shakeTrigger = !this.shakeTrigger;
+    this.flipTrigger = !this.flipTrigger;
   }
 
 }
